c8beb4be7a4d544fd408c3c86b942186d55fc2ec
sugandv21 <sdvsuganya@gmail.com>
2025-08-27 13:55:50 +0530

    Update settings.py

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x2t43#f5m2nk_@--dl4a3)y4^ir#g*ekcf8@%m71g*j8hglkz*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -124,3 +124,4 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
